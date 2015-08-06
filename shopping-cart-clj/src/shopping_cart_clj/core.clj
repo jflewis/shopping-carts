7f4dd51c522f5372c45926199a6f01a9dcab6291
John Lewis <jlewis@ksmpartners.com>
2015-08-06 08:08:05 -0400

    Created the clojure shopping cart program, Inital push

---
+++ shopping-cart-clj/src/shopping_cart_clj/core.clj
@@ -0,0 +1,7 @@
+(ns shopping-cart-clj.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ shopping-cart-clj/test/shopping_cart_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns shopping-cart-clj.core-test
+  (:require [clojure.test :refer :all]
+            [shopping-cart-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
